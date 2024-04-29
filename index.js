ee9567bb004345ef7075f2d0a639849be1b467f8
Mariavalonga <maria.valonga@aluno.senai.br>
2024-04-29 14:48:18 -0300

    feat: finalização do código

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const app = express();
 const port = 4000;
 
-// Configuração do pool de conexão com o PostgreSQL
+
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
@@ -13,9 +13,20 @@ const pool = new Pool({
   port:  7007,
 });
 app.use(express.json());
-
 app.post('/bruxos', async (req, res) => {
     const { nome, idade, casa, habilidade, status_sangue } = req.body;
+
+
+    const tiposSangueValidos = ['trouxa', 'mestiço', 'sangue puro'];
+    if (!tiposSangueValidos.includes(status_sangue)) {
+        return res.status(400).json({ error: 'Tipo de sangue inválido. Escolha entre trouxa, mestiço ou sangue puro.' });
+    }
+
+    const tiposCasasValidas = ['Grifinória', 'Sonserina', 'Lufa-Lufa', 'Corvinal' ];
+    if (!tiposCasasValidas.includes(casa)) {
+        return res.status(400).json({ error: 'Casa inválida, escolha entre Grifinória, Sonserina, Lufa-Lufa ou Corvinal' });
+    }
+
     const query = 'INSERT INTO bruxos (nome, idade, casa, habilidade, status_sangue) VALUES ($1, $2, $3, $4, $5) RETURNING *';
     const values = [nome, idade, casa, habilidade, status_sangue];
 
@@ -48,7 +59,7 @@ app.get('/bruxos', async (req, res) => {
         try {
             const result = await pool.query(query, values);
             if (result.rowCount === 0) {
-                // Se nenhum registro foi atualizado, significa que o ID fornecido não corresponde a nenhum registro existente
+        
                 res.status(404).json({ error: 'Bruxo não encontrado' });
             } else {
                 res.status(200).json({ message: 'Bruxo atualizado com sucesso', updatedBruxo: result.rows[0] });
@@ -73,6 +84,45 @@ app.get('/bruxos', async (req, res) => {
         res.status(500).json({ error: 'Erro ao excluir o bruxo' });
     }
     });
+
+
+    app.get('/varinhas', async (req, res) => {
+        try {
+            const result = await pool.query('SELECT * FROM varinhas');
+            res.json(result.rows);
+        } catch (error) {
+            console.error('Erro ao buscar as varinhas:', error);
+            res.status(500).json({ error: 'Erro ao buscar as varinhas' });
+        }
+        });
+
+        app.put('/varinhas/:id', async (req, res) => {
+            const id = req.params.id;
+            const { material, comprimento, nucleo, data_fabricacao } = req.body;
+            const query = 'UPDATE varinhas SET material = $1, comprimento = $2, nucleo = $3, data_fabricacao = $4 WHERE id = $5 RETURNING *';
+            const values = [material, comprimento, nucleo, data_fabricacao, id];
+        
+        try {
+            await pool.query(query, values);
+            res.send('Varinha atualizada');
+
+        } catch (error) {
+            console.error('Erro ao atualizar a varinha:', error);
+            res.status(500).send( 'Erro ao atualizar a varinha' );
+        }
+        });
+
+        app.delete('/varinhas/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = 'DELETE FROM varinhas WHERE id = $1';
+            try {
+                await pool.query(query, [id]);
+                res.send('Varinha excluída');
+            } catch (error) {
+                console.error('Erro ao excluir a varinha:', error);
+                res.status(500).json({ error: 'Erro ao excluir a varinha' });
+            }
+        });
 
     
 app.listen(port, () => {
